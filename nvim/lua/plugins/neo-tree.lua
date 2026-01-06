7141c94eb69888aac4da4a537b048c6ff3f79081
Jake Huggart <j.huggart@fetchrewards.com>
2026-01-06 16:49:39 -0600

    Add neo-tree file explorer and fix treesitter highlighting
    - Add neo-tree plugin with <leader>e to toggle, <leader>o to focus
    - Enable treesitter syntax highlighting via vim.treesitter.start()
    - Document new keybindings in README

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  keys = {
+    { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle file explorer" },
+    { "<leader>o", "<cmd>Neotree focus<cr>", desc = "Focus file explorer" },
+  },
+  opts = {
+    filesystem = {
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+    },
+    window = {
+      position = "left",
+      width = 35,
+    },
+  },
+}
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,5 +3,11 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter").install({ "go", "gomod", "gosum", "lua", "vim", "vimdoc", "markdown" })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "*",
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
   end,
 }
