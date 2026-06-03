bc0ab16c157b96c23006a5dada50425640ca1a14
Jake Huggart <jake@jakehuggart.com>
2026-06-03 15:32:26 -0500

    nvim: add ruby + embedded_template treesitter parsers

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,9 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter").install({ "go", "gomod", "gosum", "lua", "vim", "vimdoc", "markdown" })
+    require("nvim-treesitter").install({ "go", "gomod", "gosum", "lua", "vim", "vimdoc", "markdown", "ruby", "embedded_template" })
+    -- ERB files use the `eruby` filetype; point it at the embedded_template parser
+    vim.treesitter.language.register("embedded_template", "eruby")
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "*",
       callback = function()
